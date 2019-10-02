50e0ba0329c9936086a4f647870a7673ed424e0a
Lessllie Real <rlessllie@yahoo.com>
2019-10-02 07:28:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,29 @@
 def create_an_empty_array
-  
+  array=[]
 end
 
 def create_an_array
-  
+  array=["up","left","right","bottom"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+    array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.pop("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
